19c3ff65f8dbafb88ccadcd655d2ccb4e5191d0c
Mamba1099 <mambakaranja240@gmail.com>
2023-10-30 21:42:22 +0300

    test: new test for testing multiple perimeters

--- tests/test_sq.py
+++ tests/test_sq.py
@@ -7,3 +7,8 @@ import source.shapes as shapes
 @pytest.mark.parametrize("side_length, expected_area", [(2, 4),(8, 64), (5, 25), (7, 49)])
 def test_multiple_square_area(side_length, expected_area):
     assert shapes.Square(side_length).area() == expected_area
+    
+    
+@pytest.mark.parametrize("side_length, expected_perimeter", [(2, 8),(8, 32), (5, 20), (7, 28)])
+def test_multiple_square_perimeter(side_length, expected_perimeter):
+    assert shapes.Square(side_length).perimeter() == expected_perimeter
