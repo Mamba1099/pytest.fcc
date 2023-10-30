9b3308e2eef3ec1844e077fe1664481ede5e84f0
Mamba1099 <mambakaranja240@gmail.com>
2023-10-30 21:19:21 +0300

    feat: used parametrize test function to run multiple test

--- source/shapes.py
+++ source/shapes.py
@@ -48,3 +48,12 @@ class Rectangle(Shapes):
 
     def perimeter(self):
         return (self.length + self.width) * 2
+    
+
+# super is used to call the parent class of the constructor 'Rectangle'
+#super().__init(side_length, side_length) it passes side_lenght as both the width and length when initializing a 'Rectangle' object
+# by using super you re-use the 'Rectangle' class constructor ensuring that the square is always a rectangle with equal sides
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
