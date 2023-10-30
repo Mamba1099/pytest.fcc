a6596824d98287bbb82435f2f71b273db3e62ff1
Mamba1099 <mambakaranja240@gmail.com>
2023-10-30 18:31:09 +0300

    test: create test function to an approximate comparison

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -4,8 +4,6 @@ from source.shapes import Circle
 
 
 class TestCircle:
-    # in a class based test it comes with two functions
-    # first one is setup and second one is teardown
     def setup_method(self, method):
         print(f"setting up {method}")
 
@@ -13,11 +11,15 @@ class TestCircle:
         print(f"tearing down {method}")
 
     def test_area(self):
-        self.circle = Circle(2)
-        result = self.circle.area()
-        assert result == 12.566370614359172
+        circle = Circle(2)
+        result = circle.area()
+        assert result == pytest.approx(12.57, 0.01)
 
     def test_perimeter(self):
-        self.circle = Circle(5)
-        result = self.circle.perimeter()
-        assert result == 31.41592653589793
+        circle = Circle(5)
+        result = circle.perimeter()
+        assert result == pytest.approx(31.42, 0.01)
+
+    def test_not_same_area_rectangle(self, my_rectangle):
+        circle = Circle(2)
+        assert circle.area() != my_rectangle.area()
